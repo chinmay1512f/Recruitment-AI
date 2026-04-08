92a386d3f29fc313687d8247b66f32b148ce29a9
chinmay1512f <chinmayjandrapalli68@gmail.com>
2026-04-08 12:08:07 +0530

    Delete entrypoint.py

--- entrypoint.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-import sys
-
-port = os.environ.get("PORT", "8000")
-cmd = ["gunicorn", "app:app", "--bind", f"0.0.0.0:{port}", "--workers", "1"]
-sys.exit(subprocess.call(cmd))
